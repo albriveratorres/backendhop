7e2021757cc65e9b137da1a4dfff3595d4f0f374
Eddy Rivera <eddy.riv.pass@gmail.com>
2023-03-08 13:02:42 -0600

    agregar puerto de producion

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # backendhop
+
+# install
+
+ - version de nmp v16.15.0
+ - npm install
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routerApi = require('./src/routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./src/middleware/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 const whiteList = ['http://localhost:3000'];
